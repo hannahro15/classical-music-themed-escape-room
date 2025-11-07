4f417bdc34967c9eb7f6f26fbd71999450ed8b0f
Hannah Olbrich <hannaholbrich@users.noreply.github.com>
2025-11-07 15:14:35 +0000

    Add some basical styling to the home page

--- music-escape-room/src/App.jsx
+++ music-escape-room/src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
 
 function App() {
-
-  return (
+    return (
     <>
       <h1>Classical Music Themed Escape room</h1>
       <p>Please click enter to select a room and to begin your adventure!</p>
-      <button>Enter</button>
+      <button id="enter-btn">Enter</button>
     </>
   )
-}
-
+};
 export default App
