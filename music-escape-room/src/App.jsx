6f38cfbdeb0329ee3e29569745e85f4706e2f75b
Hannah Olbrich <hannaholbrich@users.noreply.github.com>
2025-11-07 23:36:34 +0000

    Set up a couple of basic routes

--- music-escape-room/src/App.jsx
+++ music-escape-room/src/App.jsx
@@ -1,12 +1,27 @@
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import RoomsGrid from './rooms-grid.jsx'
 
-function App() {
-    return (
+// Create a Home component
+function HomePage() {
+  return (
     <>
       <h1>Classical Music Themed Escape room</h1>
       <p>Please click enter to select a room and to begin your adventure!</p>
-      <button id="enter-btn">Enter</button>
+      <Link to="/rooms-grid">
+        <button id="enter-btn">Enter</button>
+      </Link>
     </>
   )
-};
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/rooms" element={<RoomsGrid />} />
+    </Routes>
+  )
+}
+
 export default App
