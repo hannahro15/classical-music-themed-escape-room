3502172ac78f583cbc086d0e525224eed427fb5f
Hannah Olbrich <hannaholbrich@users.noreply.github.com>
2025-11-07 14:41:22 +0000

    Add basic initial content to home page

---
+++ music-escape-room/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Classical Music Themed Escape room</h1>
+      <p>Please click enter to select a room and to begin your adventure!</p>
+      <button>Enter</button>
+    </>
+  )
+}
+
+export default App
