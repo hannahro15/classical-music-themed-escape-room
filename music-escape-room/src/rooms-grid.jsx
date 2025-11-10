8af37095586e2712b6b2ff2fed4e97e8475a066d
Hannah Olbrich <hannaholbrich@users.noreply.github.com>
2025-11-10 21:15:09 +0000

    Amend some file names

--- music-escape-room/src/rooms-grid.jsx
+++ music-escape-room/src/rooms-grid.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import './rooms-grid.css'
-import rooms from './data/room-card-info.jsx'
+import rooms from './data/room-card-info.js'
 
 function RoomsGrid() {
   return (
