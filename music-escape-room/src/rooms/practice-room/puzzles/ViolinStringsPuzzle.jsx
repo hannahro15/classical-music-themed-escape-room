c1762be999b40330fc4f1f3675d18bee708b5d91
Hannah Olbrich <hannaholbrich@gmail.com>
2025-12-03 17:37:41 +0000

    Add basic violin to violin strings puzzle

--- music-escape-room/src/rooms/practice-room/puzzles/ViolinStringsPuzzle.jsx
+++ music-escape-room/src/rooms/practice-room/puzzles/ViolinStringsPuzzle.jsx
@@ -1,8 +1,30 @@
 import '../practice-room.css'
+import './ViolinStringsPuzzle.css'
 
 function ViolinStringsPuzzle() {
   return (
     <div className="violin-strings-puzzle">
+      <div className="violin-container">
+        <div className="violin-scroll"></div>
+        <div className="violin-neck"></div>
+        <div className="violin-fingerboard"></div>
+        <div className="violin-body-upper"></div>
+        <div className="violin-waist"></div>
+        <div className="violin-body-lower"></div>
+        
+        <div className="f-hole-left"></div>
+        <div className="f-hole-right"></div>
+        
+        <div className="violin-bridge"></div>
+        
+        <div className="violin-string string-1"></div>
+        <div className="violin-string string-2"></div>
+        <div className="violin-string string-3"></div>
+        <div className="violin-string string-4"></div>
+        
+        <div className="violin-tailpiece"></div>
+        <div className="violin-chinrest"></div>
+      </div>
         E A D G
     </div>
   );
